bd6bfa8bf8e508b353541c6a6c46ee8539123444
Alussius Aloy <aaloy@my.centennialcollege.ca>
2021-02-13 16:46:13 -0500

    added mainCamera Script to make camera follow the player

---
+++ Assets/_Scripts/MainCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Transform Player;
+    public UnityEngine.Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - Player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UnityEngine.Vector3 newPosition = Player.position + _cameraOffset;
+
+        transform.position = UnityEngine.Vector3.Slerp(transform.position, newPosition, SmoothFactor);
+    }
+}
